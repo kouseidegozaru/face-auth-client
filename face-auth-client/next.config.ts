1ed449bc40e1793d1f0b0f841c567d9ccff993d9
kouseidegozaru <gguangchui@gmail.com>
2024-11-29 11:08:12 +0900

    styled-componentsのインストール

--- face-auth-client/next.config.ts
+++ face-auth-client/next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
+
 export default nextConfig;
