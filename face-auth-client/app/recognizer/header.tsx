e5d4f88b47f11868c53fcac07938d1605bfee738
kouseidegozaru <gguangchui@gmail.com>
2024-11-30 12:26:04 +0900

    ヘッダーの枠組みの実装

--- face-auth-client/app/recognizer/header.tsx
+++ face-auth-client/app/recognizer/header.tsx
@@ -4,3 +4,12 @@ import TitleIcon from "../../public/User check.svg"
 import UserIcon from "../../public/User.svg"
 import {Button} from "../components/buttons"
 
+
+const HeaderContainer = ({ children }) => {
+    return (
+        <div className="fixed bg-foreground h-[100px] w-full z-[100]">
+            {children}
+        </div>
+    );
+};
+
