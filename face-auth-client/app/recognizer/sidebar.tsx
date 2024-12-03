e7a91a3c4836fc90105d5393e652deccbe1f8a0d
kouseidegozaru <gguangchui@gmail.com>
2024-12-03 12:26:36 +0900

    サイドバーの枠組みの実装

--- face-auth-client/app/recognizer/sidebar.tsx
+++ face-auth-client/app/recognizer/sidebar.tsx
@@ -4,3 +4,12 @@ import ImageIcon from "../../public/Image.svg";
 import { useState } from "react";
 import { Button } from "../components/buttons";
 
+function SidebarContainer({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="w-[200px] h-full bg-foreground border-r-2 border-line flex flex-col z-[99]">
+            {children}
+        </div>
+    );
+}
+
+
