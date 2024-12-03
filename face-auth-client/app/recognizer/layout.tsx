18ccbee893823ec14e7948336d93887c0c6b8cb6
kouseidegozaru <gguangchui@gmail.com>
2024-12-03 14:11:42 +0900

    メインのページを表示する枠組みの作成

--- face-auth-client/app/recognizer/layout.tsx
+++ face-auth-client/app/recognizer/layout.tsx
@@ -3,3 +3,15 @@
 import Header from "./header";
 import Sidebar from "./sidebar";
 
+
+function MainBody({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex-1 bg-background h-full flex flex-col items-center justify-center">
+            <main className="bg-foreground w-[95%] h-[95%] rounded-md shadow-md">
+                {children}
+            </main>
+            
+        </div>
+    );
+}
+
