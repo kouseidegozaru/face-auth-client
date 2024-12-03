9920cf02627e28f0eab807b9a2522d19fce15ccc
kouseidegozaru <gguangchui@gmail.com>
2024-12-03 12:25:28 +0900

    サイドバーに必要な要素をインポート

---
+++ face-auth-client/app/recognizer/sidebar.tsx
@@ -0,0 +1,6 @@
+import AddGroupIcon from "../../public/Add Group.svg";
+import GroupIcon from "../../public/Group.svg";
+import ImageIcon from "../../public/Image.svg";
+import { useState } from "react";
+import { Button } from "../components/buttons";
+
