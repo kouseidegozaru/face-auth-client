15ee51f8ba8d774fe16a334e15f4b1af7c5b1f20
kouseidegozaru <gguangchui@gmail.com>
2024-12-09 16:11:11 +0900

    子要素も角が丸くなるように修正

--- face-auth-client/app/accounts/layout.tsx
+++ face-auth-client/app/accounts/layout.tsx
@@ -2,7 +2,7 @@ import Header from "./header"
 
 function MainBody({ children }: { children: React.ReactNode }) {
     return (
-        <main className="bg-foreground w-[500px] h-[500px] rounded-[30px] border-2 border-line">
+        <main className="bg-foreground w-[500px] h-[500px] rounded-[30px] border-2 border-line overflow-hidden">
             {children}
         </main>
     );
