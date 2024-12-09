b931a946cc16413368e26050616ab5697f3a48e3
kouseidegozaru <gguangchui@gmail.com>
2024-12-09 14:48:10 +0900

    メッセージの背景の作成

--- face-auth-client/app/_components/message.tsx
+++ face-auth-client/app/_components/message.tsx
@@ -1,2 +1,10 @@
 import MessageIcon from "../../public/Message.svg";
 import CloseIcon from "../../public/Close.svg";
+
+const BackGround = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="w-screen h-screen bg-msg_background opacity-85 flex items-center justify-center z-1001 inset-0 fixed">
+            {children}
+        </div>
+    );
+};
