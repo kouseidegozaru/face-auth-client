75fd569cb76bd4729a2c0fdb38d67d764f8c62fe
kouseidegozaru <gguangchui@gmail.com>
2024-12-09 15:28:04 +0900

    ヘッダーの大枠の作成

--- face-auth-client/app/accounts/header.tsx
+++ face-auth-client/app/accounts/header.tsx
@@ -1,3 +1,12 @@
 import TitleIcon from "../../public/User check.svg"
 import {Button} from "../_components/buttons"
 
+
+const HeaderContainer = ({ children }) => {
+    return (
+        <div className="fixed bg-foreground h-[60px] w-full z-[100] border-b-2 border-line">
+            {children}
+        </div>
+    );
+};
+
