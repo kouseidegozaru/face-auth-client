272c9483c3334d7e4515ab22987b147ffb354457
kouseidegozaru <gguangchui@gmail.com>
2024-12-10 17:13:56 +0900

    ヘッダーの作成

--- face-auth-client/app/accounts/login/page.tsx
+++ face-auth-client/app/accounts/login/page.tsx
@@ -3,3 +3,12 @@
 import { useState } from "react"
 import { Button } from "../../_components/buttons"
 
+
+const Header = () => {
+    return (
+        <div className="bg-foreground h-[50px] w-full border-b border-line flex items-center">
+            <h4 className="ml-[30px] font-bold">ログイン</h4>
+        </div>
+    );
+}
+
