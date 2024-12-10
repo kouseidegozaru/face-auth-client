8053eecb0ba8bad2c4de0500e6d49e7000b84bbb
kouseidegozaru <gguangchui@gmail.com>
2024-12-10 17:14:58 +0900

    共通する入力欄の作成

--- face-auth-client/app/accounts/login/page.tsx
+++ face-auth-client/app/accounts/login/page.tsx
@@ -12,3 +12,22 @@ const Header = () => {
     );
 }
 
+const CustomInput = ({ label, inputValue, setInputValue, errorMessage }) => {
+    const onChange = (e) => {
+        setInputValue(e.target.value)
+    }
+    return (
+        <div className="w-[350px] h-auto bg-foreground">
+            <p className="text-[11px] text-subtext ml-2 mb-1 font-bold">{label}</p>
+            <input 
+                type="text" 
+                value={inputValue} 
+                onChange={onChange} 
+                className={`w-full h-[40px] bg-foreground rounded-[10px] border px-2 text-[14px] ${
+                    errorMessage ? "border-error" : "border-line"
+                }`} 
+            />
+            {errorMessage && <p className="text-error text-xs mt-1 ml-2">{errorMessage}</p>}
+        </div>
+    );
+}
