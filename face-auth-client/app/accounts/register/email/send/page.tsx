14919984b7835e3520cf3a07d4008c9cd50696aa
kouseidegozaru <gguangchui@gmail.com>
2024-12-10 14:18:38 +0900

    ヘッダーの作成

--- face-auth-client/app/accounts/register/email/send/page.tsx
+++ face-auth-client/app/accounts/register/email/send/page.tsx
@@ -1,2 +1,10 @@
 import { RegisterEmailResendLink } from '../../../../_links/accounts';
 
+const Header = () => {
+    return (
+        <div className="bg-foreground h-[50px] w-full border-b border-line flex items-center">
+            <h4 className="ml-[30px] font-bold">メール送信</h4>
+        </div>
+    );
+}
+
