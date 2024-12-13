5afb9da44dbc20ae2eeb33a721aed071909be8a2
kouseidegozaru <gguangchui@gmail.com>
2024-12-13 14:58:44 +0900

    ×ボタンをクリックするとOKボタンがクリックされるように変更

--- face-auth-client/app/accounts/logout/message.tsx
+++ face-auth-client/app/accounts/logout/message.tsx
@@ -1,17 +1,23 @@
 import  { LoginLink } from '../../_links/accounts'
 import { Message , OpenChildren} from '../../_components/message'
 import { Button } from '../../_components/buttons'
+import { useRef } from 'react'
 
 export default function LogoutMessage({ isOpen }: { isOpen: boolean }) {
 
+    // ×ボタンでLoginLinkをクリックするため
+    const linkRef : any = useRef()
+    const handleProgrammaticClick = () => {
+      linkRef.current.click()
+    }
   
     return (
         <OpenChildren isOpen={isOpen}>
-            <Message closeButtonEvent={}>
+            <Message closeButtonEvent={() => handleProgrammaticClick()}>
                 <div className='h-full w-full flex items-center flex-col justify-center'>
                     <p className="m-[30px] text-sm font-bold">ログアウトしました</p>
                     <LoginLink>
-                        <Button className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground">OK</Button>
+                        <Button ref={linkRef} className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground">OK</Button>
                     </LoginLink>
                 </div>
             </Message>
