3338f23714c8cf2e6d23c49ce222f2ddbebc66ed
kouseidegozaru <gguangchui@gmail.com>
2024-12-13 16:36:13 +0900

    バリデーション機能を持った入力フォームの作成

--- face-auth-client/app/accounts/password/reset/page.tsx
+++ face-auth-client/app/accounts/password/reset/page.tsx
@@ -12,6 +12,72 @@ const Header = () => {
     );
 }
 
+const ContentContainer = () => {
+    const [password1, setPassword1] = useState("")
+    const [password2, setPassword2] = useState("")
+    const [errors, setErrors] : any = useState({})
+
+    // バリデーション
+    const validate = (field, value) => {
+        const newErrors : any = { ...errors }
+
+        // フィールドごとのバリデーション
+        switch (field) {
+            case "password1":
+                if (!value) {
+                    newErrors.password1 = "パスワードを入力してください"
+                } else if (value.length < 8) {
+                    newErrors.password1 = "パスワードは8文字以上にしてください"
+                } else {
+                    delete newErrors.password1
+                }
+                break
+            case "password2":
+                if (!value) {
+                    newErrors.password2 = "パスワードを入力してください"
+                } else if (value !== password1) {
+                    newErrors.password2 = "パスワードが一致しません"
+                } else {
+                    delete newErrors.password2
+                }
+                break
+            default:
+                break
+        }
+
+        setErrors(newErrors)
+    }
+
+    return (
+        <div className="w-full h-[calc(100%-50px)] flex flex-col items-center justify-evenly">
+            <p className="text-sm font-bold m-4">新しいパスワードを入力してください</p>
+            <CustomInput 
+                label="パスワード" 
+                inputValue={password1} 
+                setInputValue={(value) => { setPassword1(value); validate("password1", value) }} 
+                errorMessage={errors.password1} 
+            />
+            <CustomInput 
+                label="パスワード(確認用)" 
+                inputValue={password2} 
+                setInputValue={(value) => { setPassword2(value); validate("password2", value) }} 
+                errorMessage={errors.password2} 
+            />
+            <Button 
+                className="w-[350px] h-[50px] text-sm text-foreground bg-primary1 hover:bg-primary1_hover" 
+                onClick={() => {
+                    if (Object.keys(errors).length === 0 && password1 && password2) {
+                        // TODO: 登録処理(ajaxで実装する)
+                    } else {
+                        alert("正しい情報を入力してください")
+                    }
+                }}
+            >
+                完了
+            </Button>
+        </div>
+    );
+}
 
 const CustomInput = ({ label, inputValue, setInputValue, errorMessage }) => {
     const onChange = (e) => {
