ae085045eb49621439c239b1d1fbd9b7e894eaf2
kouseidegozaru <gguangchui@gmail.com>
2024-12-13 15:47:14 +0900

    ヘッダーの作成

--- face-auth-client/app/accounts/password/reset/page.tsx
+++ face-auth-client/app/accounts/password/reset/page.tsx
@@ -3,3 +3,11 @@
 import { useState } from "react"
 import { Button } from "../../../_components/buttons"
 
+const Header = () => {
+    return (
+        <div className="bg-foreground h-[50px] w-full border-b border-line flex items-center">
+            <h4 className="ml-[30px] font-bold">パスワード変更</h4>
+        </div>
+    );
+}
+
