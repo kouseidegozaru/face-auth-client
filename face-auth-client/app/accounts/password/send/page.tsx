dcc1d4572145835b52a119dfd1fba1bfec59161d
kouseidegozaru <gguangchui@gmail.com>
2024-12-13 15:58:58 +0900

    ヘッダーの作成

--- face-auth-client/app/accounts/password/send/page.tsx
+++ face-auth-client/app/accounts/password/send/page.tsx
@@ -1,2 +1,10 @@
 import { PasswordResetLink } from '../../../_links/accounts';
 
+const Header = () => {
+    return (
+        <div className="bg-foreground h-[50px] w-full border-b border-line flex items-center">
+            <h4 className="ml-[30px] font-bold">パスワード変更</h4>
+        </div>
+    );
+}
+
