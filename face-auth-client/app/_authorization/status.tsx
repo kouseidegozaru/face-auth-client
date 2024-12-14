1c67b5bc0f15ff391062bc9b0a7930e2177fedac
kouseidegozaru <gguangchui@gmail.com>
2024-12-14 16:12:14 +0900

    ユーザーのログイン状態をチェックする関数の作成

---
+++ face-auth-client/app/_authorization/status.tsx
@@ -0,0 +1,21 @@
+import { getUser } from "../_api/accounts"
+import { getSessionToken } from "../_authorization/session"
+
+// ユーザーがログインしていてるかを確認
+export default async function IsLogin(): Promise<boolean> {
+    // セッショントークンを取得
+    const sessionToken = getSessionToken();
+    // セッショントークンがnullの場合はfalseを返す
+    if (sessionToken == null) {
+        return false;
+    }
+
+    try {
+        // ユーザー情報が正常に取得できた場合はtrueを返す
+        const response = await getUser(sessionToken);
+        return response.ok;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
