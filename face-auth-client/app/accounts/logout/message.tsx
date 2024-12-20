cfb23dc8353a24689ca3dffa3b1f91c97f777d11
kouseidegozaru <gguangchui@gmail.com>
2024-12-20 10:52:40 +0900

    直接ページ遷移が可能になったので処理方法を変更

--- face-auth-client/app/accounts/logout/message.tsx
+++ face-auth-client/app/accounts/logout/message.tsx
@@ -1,23 +1,16 @@
 import  { LoginPage } from '../../_links/accounts'
 import { Message , OpenChildren} from '../../_components/message'
 import { Button } from '../../_components/buttons'
-import { useRef } from 'react'
 
 export default function LogoutMessage({ isOpen }: { isOpen: boolean }) {
 
-    // ×ボタンでLoginLinkをクリックするため
-    const linkRef : any = useRef()
-    const handleProgrammaticClick = () => {
-      linkRef.current.click()
-    }
-  
     return (
         <OpenChildren isOpen={isOpen}>
-            <Message closeButtonEvent={() => handleProgrammaticClick()}>
+            <Message closeButtonEvent={() => LoginPage.Redirect()}>
                 <div className='h-full w-full flex items-center flex-col justify-center'>
                     <p className="m-[30px] text-sm font-bold">ログアウトしました</p>
                     <LoginPage.Link>
-                        <Button ref={linkRef} className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground">OK</Button>
+                        <Button className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground">OK</Button>
                     </LoginPage.Link>
                 </div>
             </Message>
