28ceb3762a7a019874c75d8ed2d304a608e6ea59
kouseidegozaru <gguangchui@gmail.com>
2024-12-26 13:50:40 +0900

    必要な機能のインポート

---
+++ face-auth-client/app/recognizer/group/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import LearningIcon from '../../../public/Learning.svg'
+import EditIcon from '../../../public/Edit.svg'
+import TrashIcon from '../../../public/Trash.svg'
+import GroupIcon from '../../../public/Group.svg'
+import { Button } from '@/app/_components/buttons'
+
