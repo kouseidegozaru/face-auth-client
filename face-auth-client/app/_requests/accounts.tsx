bad39561a2b493aba11714201f472ec9113017c9
kouseidegozaru <gguangchui@gmail.com>
2024-12-27 14:03:24 +0900

    トークンの返却方法を修正

--- face-auth-client/app/_requests/accounts.tsx
+++ face-auth-client/app/_requests/accounts.tsx
@@ -101,6 +101,9 @@ export async function getUser(sessionToken: string) {
 }
 
 // csrfトークンの発行
-export async function getCSRFToken() {
-    return requestApi('/csrf-token/', 'GET', {});
+export async function getCSRFToken() : Promise<string | null> {
+    const response = await requestApi('/csrf-token/', 'GET', {});
+    const data = await response.json();
+    const { csrfToken } = data;
+    return csrfToken
 }
