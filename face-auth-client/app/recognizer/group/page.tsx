b46dab0d6d30fb529371a3b1fa2bbe66bd30fc22
kouseidegozaru <gguangchui@gmail.com>
2024-12-27 14:05:18 +0900

    グループの取得リクエストの作成

--- face-auth-client/app/recognizer/group/page.tsx
+++ face-auth-client/app/recognizer/group/page.tsx
@@ -6,6 +6,10 @@ import TrashIcon from '../../../public/Trash.svg'
 import GroupIcon from '../../../public/Group.svg'
 import { Button } from '@/app/_components/buttons'
 import { GroupDataPage } from '@/app/_links/recognizer'
+import { GetTrainingGroup } from '@/app/_requests/recongnizer'
+import { useEffect, useState } from 'react'
+import { GetSessionToken } from '@/app/_requests/cookie'
+import { useMessageModal } from '@/app/_components/MessageModal'
 
 export default function Page() {
     return (
@@ -53,15 +57,44 @@ function ContentContainer({ children }: { children: React.ReactNode }) {
 }
 
 function GroupList() {
+    type Group = { id: string, name: string, updated_at: string };
+    const { showModal, Modal } = useMessageModal();
+    const [groups, setGroups] = useState<Group[]>([]);
+
+    const loadData = async () => {
+        // セッショントークンを取得
+        const sessionToken = await GetSessionToken();
+        if (sessionToken == null) {
+            showModal("ログインしてください", "error", 4000);
+            return;
+        }
+        // グループ一覧を取得
+        const response = await GetTrainingGroup(sessionToken);
+        if (!response.ok) {
+            showModal("グループの取得に失敗しました", "error", 4000);
+            return;
+        }
+
+        // グループ一覧をセット
+        const data: Group[] = await response.json();
+        setGroups(data);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
     return (
-        <div className="w-full h-full flex flex-col flex-0 overflow-y-auto">
-            <GroupCard group_id="group_id" group_name="group_name" updated_at="2024-01-01" />
-            <GroupCard group_id="group_id" group_name="group_name" updated_at="2024-01-01" />
-        </div>
+        <>
+            <div className="w-full h-full flex flex-col flex-0 overflow-y-auto">
+                {groups.map((group) => (<GroupCard key={group.id} group_id={group.id} group_name={group.name} updated_at={group.updated_at} />))}
+            </div>
+            <Modal />
+        </>
     )
 }
 
-function GroupCard({ group_id , group_name , updated_at }: { group_id: string, group_name: string, updated_at: string }) {
+function GroupCard({ group_id, group_name, updated_at }: { group_id: string, group_name: string, updated_at: string }) {
     return (
         <div className="w-full h-[40px] min-h-[40px] bg-foreground border-b border-line flex items-center justify-between px-4 flex-0">
             <GroupDataPage.Link linkKey={group_id}>
@@ -72,7 +105,7 @@ function GroupCard({ group_id , group_name , updated_at }: { group_id: string, g
             </GroupDataPage.Link>
             <div className="flex items-center">
                 <p className="text-[13px] text-subtext">{updated_at}</p>
-            </div>          
+            </div>
             <div className="flex items-center h-full">
                 <div className='h-full w-[40px] flex items-center justify-center text-subtext'>
                     <EditIcon className="w-4 h-4 fill-none stroke-subtext stroke-2 hover:fill-line cursor-pointer" />
