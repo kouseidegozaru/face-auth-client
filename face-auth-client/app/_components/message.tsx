ec986f24effc6af4ce83914d02ed5cdf53ca02fc
kouseidegozaru <gguangchui@gmail.com>
2025-02-12 16:26:50 +0900

    z-indexの記述方法を修正

--- face-auth-client/app/_components/message.tsx
+++ face-auth-client/app/_components/message.tsx
@@ -4,7 +4,7 @@ import CloseIcon from "../../public/Close.svg";
 
 const BackGround = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="w-screen h-screen bg-msg_background opacity-85 flex items-center justify-center z-1001 inset-0 fixed">
+        <div className="w-screen h-screen bg-msg_background opacity-85 flex items-center justify-center z-[1001] inset-0 fixed">
             {children}
         </div>
     );
