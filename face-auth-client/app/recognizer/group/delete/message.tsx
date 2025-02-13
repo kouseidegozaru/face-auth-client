1d737da26bc0600c1495aeb03d8f598c49ad4dad
kouseidegozaru <gguangchui@gmail.com>
2025-02-13 10:25:32 +0900

    グループの削除リクエストの実装

--- face-auth-client/app/recognizer/group/delete/message.tsx
+++ face-auth-client/app/recognizer/group/delete/message.tsx
@@ -1,7 +1,11 @@
 import { Message , OpenChildren} from '@/app/_components/message'
 import { Button } from '@/app/_components/buttons'
+import { DeleteTrainingGroup } from '@/app/_requests/recongnizer'
+import { SessionError , CsrfTokenError } from '@/app/_requests/modules'
+import { useMessageModal } from '@/app/_components/MessageModal'
 
 export default function GroupDeleteMessage({ isOpen , closeButtonEvent , group_id , group_name }: { isOpen: boolean , closeButtonEvent: () => void , group_id: string , group_name: string }) {
+    const { showModal , Modal } = useMessageModal()
 
     return (
         <OpenChildren isOpen={isOpen}>
@@ -9,9 +13,28 @@ export default function GroupDeleteMessage({ isOpen , closeButtonEvent , group_i
                 <div className='h-full w-full flex items-center flex-col justify-center text-text'>
                     <p className="m-[30px] text-sm font-bold">グループを削除します</p>
                     <p className="m-[30px] text-sm">グループ名：{group_name}</p>
-                    <Button className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground">削除</Button>
+                    <Button className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground"
+                        onClick={async () => {
+                            try {
+                                const response = await DeleteTrainingGroup(group_id)
+                                if (response.ok) {
+                                    closeButtonEvent()
+                                } else {
+                                    showModal("グループの削除に失敗しました", "error", 4000)
+                                }
+                            } catch (e) {
+                                if (e instanceof SessionError) {
+                                    showModal("ログインしてください", "error", 4000)
+                                } else if (e instanceof CsrfTokenError) {
+                                    showModal("グループの削除に失敗しました", "error", 4000)
+                                } else {
+                                    showModal("グループの削除に失敗しました", "error", 4000)
+                                }
+                            }
+                        }}>削除</Button>
                 </div>
             </Message>
+            <Modal />
         </OpenChildren>
     )
 }
