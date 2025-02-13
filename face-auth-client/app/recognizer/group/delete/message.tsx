5314065b25dfb1da01bff0462d2c69f7052382f0
kouseidegozaru <gguangchui@gmail.com>
2025-02-13 10:14:31 +0900

    メッセージのデザインの変更

--- face-auth-client/app/recognizer/group/delete/message.tsx
+++ face-auth-client/app/recognizer/group/delete/message.tsx
@@ -6,7 +6,11 @@ export default function GroupDeleteMessage({ isOpen , closeButtonEvent , group_i
     return (
         <OpenChildren isOpen={isOpen}>
             <Message closeButtonEvent={closeButtonEvent}>
-                
+                <div className='h-full w-full flex items-center flex-col justify-center text-text'>
+                    <p className="m-[30px] text-sm font-bold">グループを削除します</p>
+                    <p className="m-[30px] text-sm">グループ名：{group_name}</p>
+                    <Button className="w-[90px] h-[45px] m-[30px] bg-primary2 hover:bg-primary2_hover text-foreground">削除</Button>
+                </div>
             </Message>
         </OpenChildren>
     )
